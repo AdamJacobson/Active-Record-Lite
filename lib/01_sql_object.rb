ff587cc56ce2bc6751a368383236b2f70e43715c
adam <amartenj@gmail.com>
2017-06-16 14:49:16 -0700

    Complete phase 1

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -20,9 +20,9 @@ class SQLObject
   end
 
   def self.finalize!
-    @columns.each do |column|
-      define_method("#{column}") { @attributes["#{column}"] }
-      define_method("#{column}=") { |value| @attributes["#{column}"] = value }
+    columns.each do |column|
+      define_method("#{column}") { attributes[column] }
+      define_method("#{column}=") { |value| attributes[column] = value }
     end
   end
 
@@ -37,19 +37,35 @@ class SQLObject
   end
 
   def self.all
-    # ...
+    rows = DBConnection.execute(<<-SQL)
+      SELECT *
+      FROM '#{table_name}'
+    SQL
+    parse_all(rows)
   end
 
   def self.parse_all(results)
-    # ...
+    results.map { |row| self.new(row) }
   end
 
   def self.find(id)
-    # ...
+    rows = DBConnection.execute(<<-SQL, id)
+      SELECT *
+      FROM '#{table_name}'
+      WHERE id = ?
+      LIMIT 1
+    SQL
+    parse_all(rows).first
   end
 
   def initialize(params = {})
-    # ...
+    columns = self.class.columns
+
+    params.each do |name, value|
+      name = name.to_sym
+      raise "unknown attribute '#{name}'" unless columns.include? name
+      self.send("#{name}=", value)
+    end
   end
 
   def attributes
@@ -57,18 +73,41 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |col| self.send(col) }
   end
 
   def insert
-    # ...
+    vals = attribute_values
+    columns = self.class.columns
+    question_marks = (["?"] * columns.length).join(', ')
+    col_names = columns.join(', ')
+
+    DBConnection.execute(<<-SQL, *vals)
+      INSERT INTO #{self.class.table_name} (#{col_names})
+      VALUES (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    vals = attribute_values
+    columns = self.class.columns
+
+    p set_row = columns.map { |col| "#{col} = ?" }.join(', ')
+
+    DBConnection.execute(<<-SQL, *vals, self.id)
+      UPDATE #{self.class.table_name}
+      SET #{set_row}
+      WHERE id = ?
+    SQL
   end
 
   def save
-    # ...
+    if self.id.nil?
+      insert
+    else
+      update
+    end
   end
 end
