3b051d242414f279ccd85dcf8060471c55e91547
adam <amartenj@gmail.com>
2017-06-16 16:46:28 -0700

    Partly completes Associatable

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,30 +10,45 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    if @class_name.nil?
+      nil
+    else
+      @class_name.constantize
+    end
   end
 
   def table_name
-    # ...
+    self.model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @primary_key = options[:primary_key] || :id
+    @foreign_key = options[:foreign_key] || "#{name}_id".underscore.to_sym
+    @class_name = options[:class_name] || name.capitalize
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @primary_key = options[:primary_key] || :id
+    @foreign_key = options[:foreign_key] || "#{self_class_name}_id".underscore.to_sym
+    @class_name = options[:class_name] || name.singularize.capitalize
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      fk = options.foreign_key
+      clazz = options.model_class
+      rows = where(id: fk)
+      rows.first
+    end
   end
 
   def has_many(name, options = {})
@@ -46,5 +61,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
